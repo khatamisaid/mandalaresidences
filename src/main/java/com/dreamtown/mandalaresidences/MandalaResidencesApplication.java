3932255b18ff00a30fdf81066ad9ebed05041799
Said Muhammad Khatami <33-said@users.noreply.gitlab.tecmint.com>
2023-07-03 21:45:12 +0700

    Mandala Residences

---
+++ src/main/java/com/dreamtown/mandalaresidences/MandalaResidencesApplication.java
@@ -0,0 +1,13 @@
+package com.dreamtown.mandalaresidences;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MandalaResidencesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MandalaResidencesApplication.class, args);
+	}
+
+}
