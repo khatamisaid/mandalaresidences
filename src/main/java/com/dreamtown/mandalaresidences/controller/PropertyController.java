0290ad92e02d3e14740640cd946d477ab007e5ad
Said Muhammad Khatami <33-said@users.noreply.gitlab.tecmint.com>
2023-07-12 12:26:04 +0700

    update

--- src/main/java/com/dreamtown/mandalaresidences/controller/PropertyController.java
+++ src/main/java/com/dreamtown/mandalaresidences/controller/PropertyController.java
@@ -2,7 +2,6 @@ package com.dreamtown.mandalaresidences.controller;
 
 import java.io.File;
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.ArrayList;
@@ -15,18 +14,15 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.env.Environment;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.util.MultiValueMap;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RequestPart;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.multipart.MultipartFile;
 
 import com.dreamtown.mandalaresidences.entity.ContactPerson;
